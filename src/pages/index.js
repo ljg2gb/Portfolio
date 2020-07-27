b51cd49e26ba6b02db0c59a44a714c1eb72d6353
Lydia Gregory <ljg2gb@virginia.edu>
2020-07-27 15:59:58 -0600

    creates boilerplate portffolio pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <h1>Lydia Gregory's Portfolio</h1>
+      <h2>Welcome, I'm glad you're here!</h2>
+      <p>I'll be launching my portfolio soon. In the meantime, you can learn more about me on <a href="https://github.com/ljg2gb">Github</a> and <a href="https://www.linkedin.com/in/lydia-gregory/">LinkedIn</a>.
+      </p>
+    </div>
+  );
 }
