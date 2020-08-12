242cb1cbe2d392f6025cfc77cc5fd35fad34c5aa
Lydia Gregory <ljg2gb@virginia.edu>
2020-08-12 09:47:47 -0400

    adds information to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,14 @@ import Nav from "../components/nav";
 import Main from "../components/headerLayout";
 
 const title = 'About Me'
-const text = 'I am a software engineer with a background in graphic design and marketing.'
-
+const text = 
+    <div>
+        <h3>I am a software engineer, lifelong learner, and salsa dancer! Before I transitioned into code, I worked in graphic design and marketing. I spent a year living in Spain - teaching English and improving my Spanish after I graduated the University of Viriginia in 2018 with a degree in Cognitive Science, and Studio Art. While at school, I pursued an interest in art and developed a personal <a href='https://ljg2gb.myportfolio.com/'>portfolio</a> of my work.</h3>
+        <br/>
+        <h3>Finding code has allowed me to express myself and my ideas in new and creative ways. I am passionate about user-experiece, and interative design. I love the challenge of solving interesting code problems, and feel proud to have chosen a career with endless opporunities to learn and grow. If you're interested in learning more about me and my transtion into tech, I'd love to hear from you! My email is lydia.gregory.dev@gmail.com.</h3>
+        <br/>
+    </div>
+    
 export default function About() {
   return (
     <div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Nav from "../components/nav";
 import Announcement from "../components/headerLayout";
 
 const title = 'Welcome'
-const text = <h3>I'm glad you're here. I'll be launching my portfolio soon! In the meantime, you can learn more about me on <a href="https://github.com/ljg2gb">Github</a> and <a href="https://www.linkedin.com/in/lydia-gregory/">LinkedIn</a>.</h3>
+const text = <h3>I'm glad you're here! I'll be launching my portfolio soon. In the meantime, you can learn more about me on <a href="https://github.com/ljg2gb">Github</a> and <a href="https://www.linkedin.com/in/lydia-gregory/">LinkedIn</a>.</h3>
 
 export default function Home() {
   return (
