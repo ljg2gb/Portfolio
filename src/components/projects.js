ca3bbce260b3977c157749f8ff9bf78e1be795ab
Lydia Gregory <ljg2gb@virginia.edu>
2020-08-13 15:11:06 -0400

    adds project content

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,9 +5,20 @@ export default function Projects() {
   return (
     <div className='projects'>
         <h2>Projects</h2>
-        <Project></Project>
-        <Project></Project>
-        <Project></Project>
+            <div className='projects-container'>
+                <Project 
+                    title='Polaroll' 
+                    text='An interactive mobile camera/photo app inspired by the polaroid camera.'
+                ></Project>
+                <Project 
+                    title='Candle' 
+                    text="A birthday reminder app to never again forget a friend's birthday."
+                ></Project>
+                <Project
+                    title='Accountabilibuddies' 
+                    text="A social media app to help accountability groups complete tasks."
+                ></Project>
+            </div>
     </div>
   );
 }
