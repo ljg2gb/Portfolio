3ccda8998bf63a2d3fe97ea0a62b3707db44b50c
Lydia Gregory <ljg2gb@virginia.edu>
2020-09-30 16:57:17 -0400

    updates all icons to be acessible with aria-labels, adds codepen llink to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,34 @@
 import React from "react";
-import { FaGithub, FaLinkedin, FaMedium, FaDev } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaMedium, FaCodepen } from 'react-icons/fa';
 
 export default function Footer() {
     return (
         <div className='footer'>
-            <a href='https://github.com/ljg2gb'>
-                <FaGithub/>
-            </a>
-            <a href='https://www.linkedin.com/in/lydia-gregory/'>
-                <FaLinkedin/>
-            </a>
-            <a href='https://medium.com/@ljg2gb'>
-                <FaMedium/>
-            </a>
-            <a href='https://dev.to/ljg2gb'>
-                <FaDev/>
-            </a>
-
+            <div>
+                <p>Built with <span role="img" aria-label="heart emoji">❤ </span> by Lydia</p>
+            </div>
+            <div>
+                <a href='https://github.com/ljg2gb'>
+                    <span role="img" aria-label="link to github">
+                        <FaGithub/>
+                    </span>
+                </a>
+                <a href='https://www.linkedin.com/in/lydia-gregory/'>
+                    <span role="img" aria-label="link to linkedIn">
+                        <FaLinkedin/>
+                    </span>
+                </a>
+                <a href='https://medium.com/@ljg2gb'>
+                    <span role="img" aria-label="link to Medium">
+                        <FaMedium/>
+                    </span>
+                </a>
+                <a href='https://codepen.io/Lydia_gregory'>
+                    <span role="img" aria-label="link to Codepen">
+                        <FaCodepen/>
+                    </span>
+                </a>
+            </div>
         </div>
     )
 }
