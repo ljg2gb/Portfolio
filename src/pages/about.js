0a2e449409d75a29e767d3baf2836bb9189c12dc
Lydia Gregory <ljg2gb@virginia.edu>
2020-11-04 15:01:15 -0500

    updates about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,11 +6,11 @@ import Footer from "../components/footer";
 const title = 'About Me'
 const text = 
     <div>
-        <h3>I am a software engineer, lifelong learner, and an avid salsa dancer. I graduated the University of Viriginia in 2018 with a degree in Cognitive Science, and Studio Art, and transitioned into code with the Flatiron School in Denver.</h3>
+        <h3>I am a software engineer, lifelong learner, and an avid salsa dancer. I graduated the University of Viriginia in 2018 with a degree in Cognitive Science and Studio Art, and transitioned into code with the Flatiron School in Denver.</h3>
         <br/>
-        <h3>Finding code has allowed me to express myself and my ideas in new and creative ways. I love the challenge of solving interesting code problems, and am excited to have chosen a career with endless opporunities to learn and grow. If you're interested hearing more about my transtion into tech, my email is <i>lydia.gregory.dev@gmail.com.</i></h3>
+        <h3>Finding code has allowed me to express myself and my ideas in new and creative ways. I love the challenge of solving interesting code problems, and am excited to have chosen a career with endless opporunities to learn and grow. If you'd like to chat, my email is <i>lydia.gregory.dev@gmail.com</i>. I'd love to hear from you.</h3>
         <br/>
-        <h3>You can see my previous design work, sculptures and photography at <a className='link' href='https://ljg2gb.myportfolio.com/'>personal portfolio</a>.</h3>
+        <h3>You can check out my design work, photography, and sculptures at <a className='link' href='https://ljg2gb.myportfolio.com/'>personal portfolio</a>.</h3>
     </div>
     
 export default function About() {
